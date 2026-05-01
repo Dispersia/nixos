6d91096511f0f51dff812a64a0fecbe836006c25
Aaron Housh <dispersias@gmail.com>
2026-04-30 18:12:41 -0700

    more

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
