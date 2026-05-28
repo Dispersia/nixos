1568bb3c5c626be73641b0cd8e7d6493a3489c88
Aaron Housh <dispersias@gmail.com>
2026-05-28 15:01:47 -0700

    hmm

--- hosts/phone/default.nix
+++ hosts/phone/default.nix
@@ -7,7 +7,7 @@
 }:
 {
   boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.initrd.availableKernelModules = [
     "virtio_blk"
