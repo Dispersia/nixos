5c47bb692178cc359bc01bdb43f2a31481c42aac
Aaron Housh <dispersias@gmail.com>
2026-06-23 17:09:00 -0700

    add arduino on linux

--- home/linux.nix
+++ home/linux.nix
@@ -7,6 +7,7 @@
     ./environment/kde
     ./environment/niri
     ./development/android
+    ./development/arduino
     ./development/bambu-studio
     ./development/freecad
     ./development/kicad
